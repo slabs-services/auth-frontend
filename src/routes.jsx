187d5575890744b861ca1a7a7b7613fb6041eaf3
Tomás Figueiredo <dev.tomas2003@gmail.com>
2026-08-31 15:53:53 +0000

    OAuth Redirects

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Auth from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import MFARegister from "./Pages/MFARegister";
@@ -9,11 +9,13 @@ import ForgotPassword from "./Pages/ForgotPassword";
 import ResetPassword from "./Pages/ResetPassword";
 import LostMFA from "./Pages/LostMFA";
 import ResetMFA from "./Pages/ResetMFA";
+import { GetMYAccountClient } from "./Utils";
 
 export default function Router(){
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to={"/oauth?" + GetMYAccountClient()} replace />} />
                 <Route path="/oauth" element={<Auth />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/add-mfa" element={<MFARegister />} />
